a16537a7f33c3ada18efe1f3e1324a0704587b9f
Sophie Y <javachiphi@gmail.com>
2023-11-04 17:57:04 +0800

    Add win & loss condition

--- src/App.js
+++ src/App.js
@@ -1,54 +1,28 @@
-import React from "react";
-import { getRandomWord } from "./utils.js";
+import React, { useState } from 'react';
+import GuessGame from "./GuessGame.js"; 
 import "./App.css";
 
-class App extends React.Component {
-  constructor(props) {
-    // Always call super with props in constructor to initialise parent class
-    super(props);
-    this.state = {
-      // currWord is the current secret word for this round. Update this with this.setState after each round.
-      currWord: getRandomWord(),
-      // guessedLetters stores all letters a user has guessed so far
-      guessedLetters: [],
-      // Insert num guesses left state here
-      // Insert form input state here
-    };
-  }
+function App() {
+  // The key state is used to force remount the GuessGame component
+  const [key, setKey] = useState(0);
 
-  generateWordDisplay = () => {
-    const wordDisplay = [];
-    // for...of is a string and array iterator that does not use index
-    for (let letter of this.state.currWord) {
-      if (this.state.guessedLetters.includes(letter)) {
-        wordDisplay.push(letter);
-      } else {
-        wordDisplay.push("_");
-      }
-    }
-    return wordDisplay.toString();
+  const resetGame = () => {
+    // By updating the key, the GuessGame component will remount
+    setKey(prevKey => prevKey + 1);
   };
 
-  // Insert form callback functions handleChange and handleSubmit here
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Guess The Word 🚀</h1>
-          <h3>Word Display</h3>
-          {this.generateWordDisplay()}
-          <h3>Guessed Letters</h3>
-          {this.state.guessedLetters.length > 0
-            ? this.state.guessedLetters.toString()
-            : "-"}
-          <h3>Input</h3>
-          {/* Insert form element here */}
-          Todo: Insert form element here
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Guess The Word 🚀</h1>
+        <h3>Word Display</h3>
+        <GuessGame key={key} resetGame={resetGame} />
+      </header>
+    </div>
+  );
 }
 
 export default App;
+
+
+
---
+++ src/GuessGame.js
@@ -0,0 +1,90 @@
+import React, {useState} from "react";
+import { getRandomWord } from "./utils.js";
+import "./App.css";
+
+///error case. if a user types more than 1 letter, or number, non-number -> disable submit button
+
+function GuessGame({resetGame}) {
+  const initialWord = getRandomWord();
+
+  const [inputValue, setInputValue] = useState('');
+  const [currWord, setCurrWord] = useState(initialWord);
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [numGuesses, setNumGuesses] = useState(10);
+  const [status, setStatus] = useState('');
+
+  const generateWordDisplay = () => {
+    return currWord
+      .split('')
+      .map(letter => guessedLetters.includes(letter) ? letter : "_")
+      .join(' ');
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleClick = () => {
+    if (!inputValue.trim()) return; // Don't do anything if the input is empty or just spaces
+    setGuessedLetters(prevGuessedLetters => [...prevGuessedLetters, inputValue]);
+    setNumGuesses(prevNumGuesses => prevNumGuesses - 1);
+    setInputValue('');
+
+    const newGuessedLetters = [...guessedLetters, inputValue];
+    checkStatus(newGuessedLetters);
+  };
+
+
+  const checkStatus = (newGuessedLetters) => {
+    const displayedWord = currWord
+    .split('')
+    .map(ele => newGuessedLetters.includes(ele)? ele : '_')
+    .join('');
+
+
+    if(!displayedWord.includes('_')){
+        setStatus('win');
+    }
+  }
+
+  const isSingleLetter = (input) => {
+    if(input === '') {
+        return false } 
+    else {
+    return /^[a-zA-Z]$/.test(input)
+}
+  };
+  
+  
+  console.log('currWord', currWord)
+  console.log(guessedLetters.join('') === currWord)
+  console.log('status', status)
+  console.log('status', status === 'win')
+  return (
+    <div className="App">
+        <div className="word-display">{generateWordDisplay()}</div>
+        <h3>Guessed Letters</h3>
+        {status === 'win' ? <h3>WOn</h3> : null}
+        <div>{guessedLetters.join(', ') || "-"}</div>
+        {numGuesses === 0 ? (
+          <div>
+            <h3>Oops, game over. Wanna Play Again?</h3>
+            <p>the correct word was {currWord}</p>
+            <button onClick={resetGame}>Play Again!</button>
+          </div>
+        ) : (
+          <>
+            <h3>Num Guesses: {numGuesses}</h3>
+            <input
+              value={inputValue}
+              onChange={handleChange}
+              disabled={numGuesses <= 0}
+            />
+            <button onClick={handleClick} disabled={numGuesses <= 0 || (inputValue !== '' && !isSingleLetter(inputValue))}>Guess</button>
+          </>
+        )}
+    </div>
+  );
+}
+
+export default GuessGame;
